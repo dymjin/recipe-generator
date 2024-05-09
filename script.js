eb2c94d9a9b58577044f7b9c3e3e8e69eadc2409
dymjin <dymjin@gmail.com>
2024-05-09 10:51:06 +0200

    Edit share link to test format

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // console.log(window.location.href)
 const submit = document.getElementById("submit");
 const title = document.getElementById("title");
-const params = new URL(window.location.href).searchParams;
+// const params = new URL(window.location.href).searchParams;
 
 function setStorage(item, src) {
   if (localStorage.getItem(item)) {
@@ -11,19 +11,16 @@ function setStorage(item, src) {
   }
 }
 
-// title.onchange = () => {
-//   setStorage("test", params.get("recipeTitle"));
-// };
-
-// title.onblur = () => {
-//   setStorage("test", params.get("recipeTitle"));
-// };
-
-
+// const whatsappLink = encodeURI("https://wa.me/?text=urlencodedtext");
 
 window.onload = () => {
-  title.value = params.get("recipeTitle");
+  // title.value = params.get("recipeTitle");
   setStorage("test", params.get("recipeTitle"));
   setStorage("recipe_link", new URL(window.location.href));
   // console.log(JSON.stringify(new URL(window.location.href)));
 };
+
+const whatsappLink = document.querySelector(".share");
+whatsappLink.href = encodeURI(
+  "_%2AAssassin%27s%20Pasta%2A_%0A%0A%2ADescription%2A%0APasta%20with%20a%20hint%20of%20suffering%20and%20pain%0A%0A%2ASteps%2A%0A1%29%20Preheat%20oven%20to%20700C.%0A2%29%20Die.%0A%0A%2AIngredients%2A%0A-%20wet%20bread%0A-%202%20ghost%20scorpion%20wet%20sock%20radioactive%20donkey%20chillies%0A%0AEnjoy%21"
+);
